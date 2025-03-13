fbb14c98cf7e71f9caf76776c4bd1db5a0e2eef9
Lee Conroy <leejconroy1977@gmail.com>
2025-03-13 15:19:30 +0000

    like and unlike routes added to comments

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -10,9 +10,11 @@ import { GroupsModule } from 'src/groups/groups.module';
 import { UsersModule } from 'src/users/users.module';
 import { AppEvent } from 'src/entities/event.entity';
 import { Like } from 'src/entities/like.entity';
+import { Notification } from 'src/entities/notification.entity';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, User, Group, AppEvent, Like]), GroupsModule, UsersModule],
+  imports: [TypeOrmModule.forFeature([Comment, User, Group, AppEvent, Like, Notification]), GroupsModule, UsersModule, NotificationsModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })

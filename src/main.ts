aedd7064832a45bc193c66a7946a163cbd4bb240
Lee Conroy <leejconroy1977@gmail.com>
2025-06-09 19:30:10 +0100

    res.cookie sameSite change to lax whilst working with local host

--- src/main.ts
+++ src/main.ts
@@ -23,9 +23,6 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Set-Cookie'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }

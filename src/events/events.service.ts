fef0a5e4a0294f9515db4e7acbd81b8b4684401f
Lee Conroy <leejconroy1977@gmail.com>
2025-06-24 18:34:17 +0100

    stipe.service and controller updated to include refund

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -537,7 +537,7 @@ export class EventsService {
               `Invalid price for event ${eventId}, priceBand ${priceBand.type}`,
             );
           } else {
-            refundAmount = Math.round(priceNumber / 100);
+            refundAmount = Math.round(priceNumber);
             priceBand.ticketCount += 1;
             updateData.priceBands = [...priceBands];
 
